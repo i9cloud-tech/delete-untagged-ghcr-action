32a3e8a20e33c4765fbcdb104057443f58b9f493
suhlimakz <suh.lima.kz@gmail.com>
2022-04-22 11:08:48 -0300

    Debug

--- src/delete-untagged-versions-ghcr.py
+++ src/delete-untagged-versions-ghcr.py
@@ -21,13 +21,15 @@ def main():
     f'''
         __________________________________________________
         |                                                |
-        |   Versions id deleted: {versions_deleted_ids}   |
+        |   Versions id deleted: {versions_deleted_ids}  |
         |                                                |
         __________________________________________________
     '''
   )
 
 def list_versions_by_package_name():
+  print( "Init function list versions" )
+
   result = requests.get(
     f'https://api.github.com/orgs/{org}/packages/container/{package_name}/versions',
     headers=headers
@@ -36,6 +38,8 @@ def list_versions_by_package_name():
   return result.json()
 
 def check_versions_untagged() :
+  print( "Init function check versions" )
+
   versions = list_versions_by_package_name()
   untagged_list_id = []
 
@@ -43,6 +47,8 @@ def check_versions_untagged() :
     tags = version['metadata']['container']['tags']
     
     if tags == [] :
+      print( "Init get versions untagged" )
+
       id = version['id'] 
       untagged_list_id.append( id )
       delete_versions_untagged( id )
@@ -50,9 +56,13 @@ def check_versions_untagged() :
   return untagged_list_id
 
 def delete_versions_untagged( id ) :
+  print( "Init function delete" )
+
   requests.delete(
     f'https://api.github.com/orgs/{org}/packages/container/{package_name}/versions/{id}',
     headers=headers
   )
 
+  print( f"Deleted { id }" )
+  
 main()
